2b35dcef89efd7f1e4d9c440336cfc59b9146c3b
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-10-11 11:32:24 +0200

    tabelle create correttamente. Per il momento le relazioni le ho fatte unidirezionali, vedrò in futuro se mettere bidirezionali

--- src/main/java/samueleCastaldo/Application.java
+++ src/main/java/samueleCastaldo/Application.java
@@ -9,7 +9,6 @@ public class Application {
     public static void main(String[] args) {
         EntityManager em = emf.createEntityManager();
 
-
         System.out.println("Hello World!");
     }
 }
