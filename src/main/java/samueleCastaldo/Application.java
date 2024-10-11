ba4ed1e1dfa441a92dec3ae8e8f71cb083c57969
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-10-11 10:51:38 +0200

    tabelle create correttamente, ripeto ho usato la strategia table per class

--- src/main/java/samueleCastaldo/Application.java
+++ src/main/java/samueleCastaldo/Application.java
@@ -9,6 +9,7 @@ public class Application {
     public static void main(String[] args) {
         EntityManager em = emf.createEntityManager();
 
+
         System.out.println("Hello World!");
     }
 }
